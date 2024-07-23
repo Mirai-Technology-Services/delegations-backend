4f81c68df2a71aad311caf33eb4d2937aa65446e
Jakub Mazur <j.mazur.nl@gmail.com>
2024-07-23 11:18:02 +0200

    Start trip insert

--- src/routes/trips.ts
+++ src/routes/trips.ts
@@ -1,6 +1,6 @@
 import Elysia from "elysia";
 import db from "../db/db";
-import { trips } from "../db/schema";
+import { stops_log, trips } from "../db/schema";
 import { eq } from "drizzle-orm";
 import { TokenData, authenticateJWT, jwtMiddleware } from "../middlewares/auth";
 
@@ -13,7 +13,8 @@ export const tripsRoutes = new Elysia({ prefix: "/trips" })
         const dbTrips = await db
           .select()
           .from(trips)
-          .where(eq(trips.user_id, user.id));
+          .where(eq(trips.user_id, user.id)) // @ts-ignore
+          .limit(10);
 
         return { body: dbTrips };
       })
@@ -28,6 +29,19 @@ export const tripsRoutes = new Elysia({ prefix: "/trips" })
 
         return {
           body: insertedTrip,
+        };
+      })
+      .post("/start", async ({ store: { user }, request }) => {
+        const newStop = await request.json();
+        newStop.user_id = user.id;
+
+        const [insertedStop] = await db
+          .insert(stops_log)
+          .values(newStop)
+          .returning();
+
+        return {
+          body: insertedStop,
         };
       })
       .get("/start-form", ({ store: { user } }) => {
