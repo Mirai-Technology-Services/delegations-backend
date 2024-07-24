238ed0c06d5c799f5c0bb83488986cd93733ed2b
Jakub Mazur <j.mazur.nl@gmail.com>
2024-07-24 16:36:12 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import { app } from "./app";
+import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
+import Elysia from "elysia";
+import { api } from "./api";
+import { logger } from "./logger";
 import { env } from "./env";
 
-app.listen(env.port, (server) => {});
+export const app = new Elysia()
+  .use(cors())
+  .use(swagger())
+  .use(logger({ includeTimestamp: true }))
+  .get("/", () => {
+    return "Server is running!";
+  })
+  .use(api)
+  .listen(env.port);
